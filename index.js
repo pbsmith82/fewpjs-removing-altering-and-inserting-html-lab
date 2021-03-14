7d07969da8a29fa8bdd67f2f0962a84a16b9c3c9
pbsmith82 <pbsmith82@gmail.com>
2021-03-14 15:14:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+main.remove()
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+
+let element = document.querySelector("h1");
+element.id = 'victory'
+
+let h1tag = document.getElementById("victory");
+h1tag.innerHTML = "Phillip is the champion!";
